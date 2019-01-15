7cc5bed530536457aa780c9c70e774e6f197f955
julesow <souleymanesow@gmail.com>
2019-01-15 16:20:00 +0000

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    # byebug
   end
 
   def new
@@ -20,4 +21,14 @@ class ArticlesController < ApplicationController
   end
 
   # add edit and update methods here
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+    # byebug
+    redirect_to article_path(@article)
+  end
 end
